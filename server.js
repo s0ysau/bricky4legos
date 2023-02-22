6655863e86b80208057a418ebb57b031e39bbe01
jerricklc@gmail.com <jerricklc@gmail.com>
2023-02-22 14:51:11 -0500

    First push to repo

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT || 8080
+
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use((req, res, next) => {
+  res.locals.data = {}
+  next()
+})
+
+app.get('/api/test', (req, res) => {
+  res.json({'eureka': 'you have found it'})
+})
+
+app.listen(PORT, () => {
+  console.log(`Getting Bricky for Legos in ${PORT}`)
+})
